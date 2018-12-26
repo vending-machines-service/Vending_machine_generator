9600c93a2127b5543c1e757579d842e8515ab4ac
Anatolii Zhloba <anatolyjloba@gmail.com>
2018-12-26 19:31:24 +0200

    2 commit

--- src/vending/m2m/VendingDataGeneratorAppl.java
+++ src/vending/m2m/VendingDataGeneratorAppl.java
@@ -6,12 +6,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class VendingDataGeneratorAppl {
-		private static final long TIME_OUT = 5000;
 
 		public static void main(String[] args) throws InterruptedException {
 			ConfigurableApplicationContext ctx = SpringApplication.run(VendingDataGeneratorAppl.class, args);
-			Thread.sleep(TIME_OUT);
-			ctx.close();
+
 		}
 
 	}
--- src/vending/m2m/service/VendingDataGenerator.java
+++ src/vending/m2m/service/VendingDataGenerator.java
@@ -1,5 +1,39 @@
 package vending.m2m.service;
 
-public class VendingDataGenerator {
+import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import vending.m2m.dto.VendingSensorData;
+
+@EnableBinding(Source.class)
+public class VendingDataGenerator {
+	
+	ObjectMapper mapper = new ObjectMapper();
+	Random random = new Random();
+	@Value("${n_sensors:18}")
+	int nSensors;
+	
+	@InboundChannelAdapter(value = Source.OUTPUT,
+			poller = @Poller(
+					fixedDelay = "${fixedDelay:60000}", 
+					maxMessagesPerPoll = "${nMessages:18}"))
+	String getSensorData() throws JsonProcessingException {
+		VendingSensorData sensor = getSensor();
+		return mapper.writeValueAsString(sensor);
+	}
+	
+	private VendingSensorData getSensor() {
+		int sensorId =0;
+		int value = 0;
+		return new VendingSensorData(nSensors, sensorId, value);
+		}
+	
 }
